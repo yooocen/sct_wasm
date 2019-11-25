396bce569535cb8babab376ef1005590e21807e3
chenyongda <605529354@qq.com>
2019-11-26 01:30:51 +0800

    更新大量的实验

--- src/lib.rs
+++ src/lib.rs
@@ -1,238 +1,182 @@
 extern crate cfg_if;
 extern crate wasm_bindgen;
 extern crate web_sys;
-
+extern crate js_sys;
+extern crate serde_wasm_bindgen;
 mod utils;
 
 use std::fmt;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
+use std::collections::{BTreeMap, HashMap};
+use serde_json::{Result, Value};
 
-pub struct Timer<'a> {
-    name: &'a str,
-}
+#[macro_use]
+extern crate serde_derive;
 
-impl<'a> Timer<'a> {
-    pub fn new(name: &'a str) -> Timer<'a> {
-        console::time_with_label(name);
-        Timer { name }
-    }
-}
-
-impl<'a> Drop for Timer<'a> {
-    fn drop(&mut self) {
-        console::time_end_with_label(self.name);
-    }
+#[derive(Serialize, Deserialize)]
+pub struct ParamData {
+    pub parmEnumList: BTreeMap<String,Vec<i32>>,
 }
 
 #[wasm_bindgen]
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
-}
-
-impl Cell {
-    fn toggle(&mut self) {
-        *self = match *self {
-            Cell::Dead => Cell::Alive,
-            Cell::Alive => Cell::Dead,
-        };
-    }
+pub struct Param {
+    parmEnumList : BTreeMap<String,Vec<i32>>,
 }
 
 #[wasm_bindgen]
-pub struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>,
-}
-
-impl Universe {
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
-    }
-
-    /// Get the dead and alive values of the entire universe.
-    pub fn get_cells(&self) -> &[Cell] {
-        &self.cells
-    }
-
-    /// Set cells to be alive in a universe by passing the row and column
-    /// of each cell as an array.
-    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
-        for (row, col) in cells.iter().cloned() {
-            let idx = self.get_index(row, col);
-            self.cells[idx] = Cell::Alive;
+impl Param {
+    pub fn new(paramEnumList: JsValue) -> Param {
+        let parmEnumList = serde_wasm_bindgen::from_value(paramEnumList).unwrap();
+        Param {
+            parmEnumList
         }
     }
-
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-
-        let north = if row == 0 {
-            self.height - 1
-        } else {
-            row - 1
-        };
-
-        let south = if row == self.height - 1 {
-            0
-        } else {
-            row + 1
-        };
-
-        let west = if column == 0 {
-            self.width - 1
-        } else {
-            column - 1
-        };
-
-        let east = if column == self.width - 1 {
-            0
-        } else {
-            column + 1
-        };
-
-        let nw = self.get_index(north, west);
-        count += self.cells[nw] as u8;
-
-        let n = self.get_index(north, column);
-        count += self.cells[n] as u8;
-
-        let ne = self.get_index(north, east);
-        count += self.cells[ne] as u8;
-
-        let w = self.get_index(row, west);
-        count += self.cells[w] as u8;
-
-        let e = self.get_index(row, east);
-        count += self.cells[e] as u8;
-
-        let sw = self.get_index(south, west);
-        count += self.cells[sw] as u8;
-
-        let s = self.get_index(south, column);
-        count += self.cells[s] as u8;
-
-        let se = self.get_index(south, east);
-        count += self.cells[se] as u8;
-
-        count
+    pub fn GetP(&self, code: JsValue) -> JsValue {
+        let paramCode : String = serde_wasm_bindgen::from_value(code).unwrap();
+        let val : &Vec<i32> = self.parmEnumList.get(&paramCode).unwrap();
+        let jsVal = serde_wasm_bindgen::to_value(&val).unwrap();
+//        console::log_1(jsVal);
+        jsVal
     }
-}
 
-/// Public methods, exported to JavaScript.
-#[wasm_bindgen]
-impl Universe {
-    pub fn tick(&mut self) {
-        // let _timer = Timer::new("Universe::tick");
+    pub fn GetPTestPerf(&self) -> JsValue{
+//        let paramCode : String = serde_wasm_bindgen::from_value(code).unwrap();
+        for i in 0..1000000 {
+              self.parmEnumList.get("PL_1");
+        }
+        serde_wasm_bindgen::to_value(self.parmEnumList.get("PL_1").unwrap()).unwrap()
 
-        let mut next = self.cells.clone();
+    }
 
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
+    pub fn fibonacci(&self, num: JsValue) -> JsValue{
+        let mut a: u32;
+        let mut b:u32 = 0;
+        let nn : u32= serde_wasm_bindgen::from_value(num).unwrap();
+        for i in 0..100000000 {
+            a = 0;
+            b = 1;
+            let n : u32 = nn;
+            // Use a and b to store the previous two values in the sequence
 
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
+            for i in 0..n {
+                // As we iterate through, move b's value into a and the new computed
+                // value into b.
+                let c = a + b;
+                a = b;
+                b = c;
             }
         }
+        serde_wasm_bindgen::to_value(&b).unwrap()
 
-        self.cells = next;
+//        console::log_1(&b.to_string().into())
     }
 
-    pub fn new() -> Universe {
-        utils::set_panic_hook();
 
-        let width = 128;
-        let height = 128;
 
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
-
-        Universe {
-            width,
-            height,
-            cells,
-        }
-    }
-
-    pub fn width(&self) -> u32 {
-        self.width
-    }
-
-    /// Set the width of the universe.
-    ///
-    /// Resets all cells to the dead state.
-    pub fn set_width(&mut self, width: u32) {
-        self.width = width;
-        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
-    }
-
-    pub fn height(&self) -> u32 {
-        self.height
-    }
-
-    /// Set the height of the universe.
-    ///
-    /// Resets all cells to the dead state.
-    pub fn set_height(&mut self, height: u32) {
-        self.height = height;
-        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
-    }
-
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
-    }
-
-    pub fn toggle_cell(&mut self, row: u32, column: u32) {
-        let idx = self.get_index(row, column);
-        self.cells[idx].toggle();
-    }
 }
 
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
 
-        Ok(())
-    }
+#[wasm_bindgen]
+pub struct Custom {
+    OwnedValue : JsValue
 }
 
 #[wasm_bindgen]
-pub fn test(x : JsValue) {
-    console::log_1(&x);
+impl Custom {
+
+    pub fn new( target:  JsValue) -> Custom {
+        let obj = Custom {
+            OwnedValue : target
+        };
+        obj
+    }
+
+
+    pub fn getJsValue(&self, property_key: JsValue ) -> JsValue{
+        let mut value: JsValue =  js_sys::Reflect::get(&self.OwnedValue, &property_key).unwrap();
+        for i in 0..1000 {
+            value = js_sys::Reflect::get(&self.OwnedValue, &property_key).unwrap();
+        }
+        value
+    }
+
+    pub fn getJsValuefromInput(&self, target : JsValue,property_key: JsValue ) -> JsValue{
+        let mut value: JsValue =  js_sys::Reflect::get(&target, &property_key).unwrap();
+        for i in 0..1000000 {
+            value = js_sys::Reflect::get(&target, &property_key).unwrap();
+        }
+        value
+    }
+}
+
+
+#[wasm_bindgen]
+pub fn get_value_from_js(value: JsValue) -> JsValue {
+    let value :ParamData = serde_wasm_bindgen::from_value(value).unwrap();
+    serde_wasm_bindgen::to_value(&value).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn tryOut1() -> JsValue{
+    serde_wasm_bindgen::to_value(&12345678).unwrap()
+}
+
+
+#[wasm_bindgen]
+pub fn tryOut2() -> JsValue{
+    let a:u64 =123456789123;
+    serde_wasm_bindgen::to_value(&a).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn tryOut3() -> JsValue{
+    serde_wasm_bindgen::to_value(&12.23).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn tryOut4() -> JsValue{
+    serde_wasm_bindgen::to_value(&123456789).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn untyped_example() -> JsValue {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(data).unwrap();
+    serde_wasm_bindgen::to_value(&v).unwrap()
+}
+
+
+#[wasm_bindgen]
+pub struct Pref {
+    parmEnumList : HashMap<String,Vec<i32>>,
+}
+
+#[wasm_bindgen]
+impl Pref {
+    pub fn new(paramEnumList: JsValue) -> Param {
+        let parmEnumList = serde_wasm_bindgen::from_value(paramEnumList).unwrap();
+        Param {
+            parmEnumList
+        }
+    }
+
+    pub fn GetPTestPerf(&self) -> JsValue{
+//        let paramCode : String = serde_wasm_bindgen::from_value(code).unwrap();
+        for i in 0..1000000 {
+            self.parmEnumList.get("PL_1");
+        }
+        serde_wasm_bindgen::to_value(self.parmEnumList.get("PL_1").unwrap()).unwrap()
+    }
 }
